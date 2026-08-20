94e7cbede7210f99106903cca810cd094e357832
Moshe Molcho <molcho.math@gmail.com>
2026-08-20 00:54:42 +0000

    lab #5

---
+++ mmolcho5a.py
@@ -0,0 +1,31 @@
+#Moshe Molcho Lab 5 a
+#Using a 1 dimensional array to print line without a loop.
+
+numbers = [ 2,4,6,8,10,12,14,16,18,20]
+
+# Could have mathed it 2n... :)
+
+print("No Loop")
+print(numbers[0])
+print(numbers[1])
+print(numbers[2])
+print(numbers[3])
+print(numbers[4])
+print(numbers[5])
+print(numbers[6])
+print(numbers[7])
+print(numbers[8])
+print(numbers[9])
+
+healthy= ["banana","zuccini","carrot","eggplant","cherry"]
+
+print("With Loop")
+
+for i in numbers:
+    print(i)
+
+for i in healthy:
+    print(i)
+
+
+
