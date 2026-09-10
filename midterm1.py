c8b9afcaae4131b716273cebe64ff876920b6875
Moshe Molcho <molcho.math@gmail.com>
2026-09-10 00:11:03 +0000

    Midterm Problem #1

---
+++ midterm1.py
@@ -0,0 +1,12 @@
+# Moshe Molcho Program #1 Midterm
+
+name = input("Please enter your name: ")
+
+number = int(input("Please enter a number: "))
+
+adend = int(input("How much would you like to add to that number?: "))
+
+total = number + adend
+
+print(name, " you entered ", number, " and added ", adend, " to get a total of ", total)
+
