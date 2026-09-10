d222b3d9b0ff7b068ce7609c06f7aeea4fb70484
Moshe Molcho <molcho.math@gmail.com>
2026-09-10 00:25:23 +0000

    Midterm Problem #2

---
+++ midterm2.py
@@ -0,0 +1,16 @@
+# Moshe Molcho Midterm Program #2
+
+def area_rectangle(width,length):
+    area = length*width
+    return area
+
+base = int(input("Enter the base of the rectangle: "))
+
+height = int(input("Enter the Height of the rectangle: "))
+
+area = area_rectangle(base,height)
+
+print("The area of the rectangle is: ",area)
+
+# The fact that units are missing is offensive to me as a math and science teacher :)
+           
